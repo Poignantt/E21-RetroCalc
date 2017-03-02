fd3247bedbae6afdbd2c7a70552e4add93bf1a4a
Poignantt <APP@DoodleJump.local>
2017-03-02 21:21:14 +0800

    preliminary calc logic

--- E21 RetroCalc/ViewController.swift
+++ E21 RetroCalc/ViewController.swift
@@ -9,13 +9,28 @@
 import UIKit
 import AVFoundation
 
+/*
+Tags
+ 10 - (+)
+ 11 - (-)
+ 12 - (x)
+ 13 - (/)
+ 14 - (=)
+*/
+
 class ViewController: UIViewController {
     
     var btnSound: AVAudioPlayer!
-
+    var digOutputValue: Float = 0.0
+    var str: String!
+    
+    @IBOutlet weak var digOutput: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        digOutput.text = "0"
         
         let path = Bundle.main.path(forResource: "btn", ofType: "wav")
         let soundURL = URL(fileURLWithPath: path!)
@@ -32,7 +47,17 @@ class ViewController: UIViewController {
     
     @IBAction func numberPressed(sender: UIButton) {
         playSound()
+        
+        
+        if digOutput.text != "0" {
+        digOutput.text = digOutput.text! + "\(sender.tag)"
+        }else{
+        digOutput.text = "\(sender.tag)"
+        }
+        
     }
+    
+    
     
         func playSound () {
             if btnSound.isPlaying {
